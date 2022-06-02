d813b91ea3e21513ac72881d8b7aca40a291850f
psurge1 <suraj.sswamy@gmail.com>
2022-06-01 23:00:58 -0500

    cleaned up extra code and spacing in sketch.js

--- sketch.js
+++ sketch.js
@@ -1,26 +1,25 @@
-var wFrame = 500;
-var hFrame = 500;
-var sliderElement;
-var imgSelector;
-var selectedImg;
+var wFrame, hFrame, sliderElement, imgSelector, selectedImg;
 var imgChoices = [];
 var selection = 0;
 
 function preload() {
-  img1 = loadJSON('imgs/img1.json');
-  img2 = loadJSON('imgs/img2.json');
-  imgChoices.push(img1);
-  imgChoices.push(img2);
+  imgChoices.push(loadJSON('imgs/img1.json'));
+  imgChoices.push(loadJSON('imgs/img2.json'));
 }
 
 function setup() {
+  wFrame = windowWidth-10;
+  hFrame = windowHeight;
   createCanvas(wFrame, hFrame);
+
   sliderElement = createSlider(1, 9, 1, 1);
+  sliderElement.position(50, 100);
+
   imgSelector = createButton("Next Image");
   imgSelector.style("border: 0");
   imgSelector.style("radius: 100px");
   imgSelector.size(100, 20);
-  imgSelector.mouseOver(changeImgH);
+  imgSelector.position(50, 50);
   imgSelector.mouseReleased(changeImg);
   selectedImg = imgChoices[0];
 }
@@ -39,36 +38,14 @@ function draw() {
     pixels[v+1]=selectedImg.img[i%selectedImg.img.length][1];
     pixels[v+2]=selectedImg.img[i%selectedImg.img.length][2];
     pixels[v+3]=255;
-
-    // pixels[v+4]=selectedImg.img[i%selectedImg.img.length][0];
-    // pixels[v+5]=selectedImg.img[i%selectedImg.img.length][1];
-    // pixels[v+6]=selectedImg.img[i%selectedImg.img.length][2];
-    // pixels[v+7]=255;
-
-    // pixels[v+wFrame]=selectedImg.img[i%selectedImg.img.length][0];
-    // pixels[v+wFrame+1]=selectedImg.img[i%selectedImg.img.length][1];
-    // pixels[v+wFrame+2]=selectedImg.img[i%selectedImg.img.length][2];
-    // pixels[v+wFrame+3]=255;
-
-    // pixels[v+wFrame+4]=selectedImg.img[i%selectedImg.img.length][0];
-    // pixels[v+wFrame+5]=selectedImg.img[i%selectedImg.img.length][1];
-    // pixels[v+wFrame+6]=selectedImg.img[i%selectedImg.img.length][2];
-    // pixels[v+wFrame+7]=255;
-    
     v+=4;
   }
 
   updatePixels();
-
 }
 
 function changeImg()
 {
   selectedImg = imgChoices[selection%imgChoices.length];
   selection++;
-}
-
-function changeImgH()
-{
-  
 }
